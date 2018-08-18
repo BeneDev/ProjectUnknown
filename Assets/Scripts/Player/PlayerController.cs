affc5c53f5858d2ff8abf18138664bc1a749ae54
Bene_Dev <seriousbenedikt@web.de>
2018-08-18 13:11:49 +0200

    First Physics
    Unpolished Physics system

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,9 +9,30 @@ public class PlayerController : MonoBehaviour {
 
     public event System.Action OnShotFired;
 
+    [SerializeField] BoxCollider2D colliderDefiningRaycasts;
+
+    struct PlayerRaycasts // To store the informations of raycasts around the player to calculate physics
+    {
+        public RaycastHit2D bottomLeft;
+        public RaycastHit2D bottomRight;
+        public RaycastHit2D upperLeft;
+        public RaycastHit2D lowerLeft;
+        public RaycastHit2D upperRight;
+        public RaycastHit2D lowerRight;
+        public RaycastHit2D top;
+    }
+    PlayerRaycasts raycasts; // Stores the actual information of the raycasts to calculate physics
+
+
     [SerializeField] float speed = 1f;
     [SerializeField] float backwardsSpeed = 1f;
+    [SerializeField] float jumpForce = 1f;
+    [SerializeField] float jumpHoldUpGain = 1f;
     Vector3 velocity;
+    bool isGrounded = false;
+    bool isOnWall = false;
+    [SerializeField] float veloYLimit = 1f;
+    [SerializeField] float gravity = 1f;
 
     PlayerInput input;
     Animator anim;
@@ -26,6 +47,8 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField] float shakeDurWhenShotFired = 0.2f;
 
+    LayerMask layersToCollideWith;
+
     #endregion
 
     #region Unity Messages
@@ -34,17 +57,21 @@ public class PlayerController : MonoBehaviour {
         input = GetComponent<PlayerInput>();
         anim = GetComponent<Animator>();
         camShake = Camera.main.GetComponent<CameraShake>();
+        int layer = LayerMask.NameToLayer("SolidObjects");
+        layersToCollideWith = 1 << layer;
 	}
 	
-	void Update ()
+	void FixedUpdate ()
     {
+        UpdateRaycasts();
+        CheckGrounded();
         if(input.Horizontal > 0f && transform.localScale.x > 0 || input.Horizontal < 0f && transform.localScale.x < 0)
         {
-            velocity.x = input.Horizontal * speed * Time.deltaTime;
+            velocity.x = input.Horizontal * speed * Time.fixedDeltaTime;
         }
         else
         {
-            velocity.x = input.Horizontal * backwardsSpeed * Time.deltaTime;
+            velocity.x = input.Horizontal * backwardsSpeed * Time.fixedDeltaTime;
         }
         ChangeDirection();
         if(input.Interact)
@@ -56,13 +83,22 @@ public class PlayerController : MonoBehaviour {
         {
             if(Time.realtimeSinceStartup >= timeWhenLastShot + equippedGun.ShotDelay)
             {
-                anim.SetTrigger("ShotFired");
-                OnShotFired();
-                timeWhenLastShot = Time.realtimeSinceStartup;
-                velocity.x += -transform.localScale.x * equippedGun.Recoil;
-                camShake.shakeAmount = equippedGun.Recoil;
-                camShake.shakeDuration = shakeDurWhenShotFired;
+                InitiateShoot();
             }
+        }
+        // Apply gravity
+        if (!isGrounded)
+        {
+            velocity += new Vector3(0, -gravity * Time.fixedDeltaTime);
+        }
+        CheckForValidVelocity();
+        if (input.Jump == 1 && isGrounded)
+        {
+            velocity.y += jumpForce * Time.fixedDeltaTime;
+        }
+        if(input.Jump == 2 && !isGrounded)
+        {
+            velocity.y += jumpHoldUpGain * Time.fixedDeltaTime;
         }
         transform.position += velocity;
     }
@@ -70,6 +106,129 @@ public class PlayerController : MonoBehaviour {
     #endregion
 
     #region Helper Methods
+
+    /// <summary>
+    /// Make sure the velocity does not violate the laws of physics in this game
+    /// </summary>
+    protected void CheckForValidVelocity()
+    {
+        // Check for ground under the player
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0;
+        }
+
+        // Checking for colliders to the sides
+        if (WallInWay())
+        {
+            velocity.x = 0f;
+        }
+
+        // Make sure, velocity in y axis does not get over limit
+        if (velocity.y >= 0 && velocity.y > veloYLimit)
+        {
+            velocity.y = veloYLimit;
+        }
+        if (velocity.y <= 0 && velocity.y < -veloYLimit)
+        {
+            velocity.y = -veloYLimit;
+        }
+
+        // Check if something is above the player and let him bounce down again relative to the force he went up with
+        if (raycasts.top && velocity.y > 0)
+        {
+            velocity.y = -velocity.y / 2;
+        }
+    }
+
+    /// <summary>
+    /// Checks if there are walls in the direction the player is facing
+    /// </summary>
+    /// <returns> True if there is a wall. False when there is none</returns>
+    protected bool WallInWay()
+    {
+        if (transform.localScale.x < 0)
+        {
+            if (raycasts.upperLeft || raycasts.lowerLeft)
+            {
+                isOnWall = true;
+                if (raycasts.upperLeft.distance < 0.45f)
+                {
+                    transform.position += Vector3.right * ((0.45f - (raycasts.upperLeft.distance)) / 5f);
+                }
+                else if (raycasts.lowerLeft.distance < 0.45f)
+                {
+                    transform.position += Vector3.right * ((0.45f - (raycasts.lowerLeft.distance)) / 5f);
+                }
+                return true;
+            }
+        }
+        else if (transform.localScale.x > 0)
+        {
+            if (raycasts.upperRight || raycasts.lowerRight)
+            {
+                isOnWall = true;
+                if (raycasts.upperRight.distance < 0.2f)
+                {
+                    transform.position += Vector3.left * ((0.25f - (raycasts.upperRight.distance)) / 5f);
+                }
+                else if (raycasts.lowerRight.distance < 0.2f)
+                {
+                    transform.position += Vector3.left * ((0.25f - (raycasts.lowerRight.distance)) / 5f);
+                }
+                return true;
+            }
+        }
+        isOnWall = false;
+        return false;
+    }
+
+    /// <summary>
+    /// Checks if the player is on the ground or not
+    /// </summary>
+    protected virtual void CheckGrounded()
+    {
+        // When the bottom raycasts hit ground
+        if (raycasts.bottomLeft || raycasts.bottomRight)
+        {
+            isGrounded = true;
+            velocity.y = 0f;
+            if (raycasts.bottomLeft.distance < 0.2f)
+            {
+                transform.position += Vector3.up * ((0.25f - (raycasts.bottomLeft.distance)) / 5f);
+            }
+            else if (raycasts.bottomRight.distance < 0.2f)
+            {
+                transform.position += Vector3.up * ((0.25f - (raycasts.bottomRight.distance)) / 5f);
+            }
+        }
+        // Otherwise the player is not grounded
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    void UpdateRaycasts()
+    {
+        Vector3 extents = colliderDefiningRaycasts.bounds.extents;
+        Vector3 center = colliderDefiningRaycasts.bounds.center;
+        raycasts.bottomRight = Physics2D.Raycast(center + new Vector3(extents.x, -extents.y, 0f), Vector2.down, 0.25f, layersToCollideWith);
+        raycasts.bottomLeft = Physics2D.Raycast(center + new Vector3(-extents.x, -extents.y, 0f), Vector2.down, 0.25f, layersToCollideWith);
+
+        raycasts.upperRight = Physics2D.Raycast(center + new Vector3(extents.x, extents.y, 0f), Vector2.right, 0.25f, layersToCollideWith);
+        raycasts.lowerRight = Physics2D.Raycast(center + new Vector3(extents.x, -extents.y, 0f), Vector2.right, 0.25f, layersToCollideWith);
+
+        raycasts.upperLeft = Physics2D.Raycast(center + new Vector3(-extents.x, extents.y, 0f), Vector2.left, 0.25f, layersToCollideWith);
+        raycasts.lowerLeft = Physics2D.Raycast(center + new Vector3(-extents.x, -extents.y, 0f), Vector2.left, 0.25f, layersToCollideWith);
+
+        raycasts.top = Physics2D.Raycast(center + new Vector3(0f, extents.y, 0f), Vector2.up, 0.25f, layersToCollideWith);
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Debug.DrawRay(colliderDefiningRaycasts.bounds.center + new Vector3(colliderDefiningRaycasts.bounds.extents.x, -colliderDefiningRaycasts.bounds.extents.y, 0f), Vector2.down * 0.75f);
+    //}
 
     private void CheckForGuns(Collider2D[] objects)
     {
@@ -103,6 +262,16 @@ public class PlayerController : MonoBehaviour {
         {
             transform.localScale = new Vector3(1f, 1f, 1f);
         }
+    }
+
+    private void InitiateShoot()
+    {
+        anim.SetTrigger("ShotFired");
+        OnShotFired();
+        timeWhenLastShot = Time.realtimeSinceStartup;
+        velocity.x += -transform.localScale.x * equippedGun.Recoil;
+        camShake.shakeAmount = equippedGun.Recoil;
+        camShake.shakeDuration = shakeDurWhenShotFired;
     }
 
     #endregion
